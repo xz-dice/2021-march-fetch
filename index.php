c040e71fa30245626b6f51391065780fc411f508
MattW-R <Matthew.wr89@gmail.com>
2021-05-10 16:38:13 +0100

    Created base html structure with placeholder articles

--- index.php
+++ index.php
@@ -11,6 +11,66 @@ require_once 'vendor/autoload.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
-
+        <header>
+            <h1>FETCH</h1>
+            <h4>An app which gives you a ruff idea of which dog is best for you.</h4>
+            <h4>You'd be barking mad not to try it.</h4>
+        </header>
+        <main>
+            <article>
+                <div>
+                    <img src="assets/dog-images/1.jpeg" />
+                </div>
+                <div>
+                    <h3>name</h3>
+                    <p>temperament</p>
+                    <p>weight</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="assets/dog-images/1.jpeg" />
+                </div>
+                <div>
+                    <h3>name</h3>
+                    <p>temperament</p>
+                    <p>weight</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="assets/dog-images/1.jpeg" />
+                </div>
+                <div>
+                    <h3>name</h3>
+                    <p>temperament</p>
+                    <p>weight</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="assets/dog-images/1.jpeg" />
+                </div>
+                <div>
+                    <h3>name</h3>
+                    <p>temperament</p>
+                    <p>weight</p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="assets/dog-images/1.jpeg" />
+                </div>
+                <div>
+                    <h3>name</h3>
+                    <p>temperament</p>
+                    <p>weight</p>
+                </div>
+            </article>
+        </main>
+        <footer>
+            <p>This page will lead to a best friend you want to stick by.</p>
+            <p>Companion app coming soon.</p>
+        </footer>
     </body>
 </html>
