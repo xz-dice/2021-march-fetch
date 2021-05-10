c06fc1e7f4c96363c8b4d2542413518e233d76de
MattW-R <Matthew.wr89@gmail.com>
2021-05-10 16:55:36 +0100

    Created styles.css & started positioning page elements.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once 'vendor/autoload.php';
     <head>
         <title>Fetch</title>
         <link href="css/normalize.css" type="text/css" rel="stylesheet" />
+        <link href="css/styles.css" type="text/css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
