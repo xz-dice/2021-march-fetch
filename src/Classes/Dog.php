a1d6e3a48a75ab4952163686c661135fae331985
xz-dice <xandicearmah@gmail.com>
2021-05-10 16:46:48 +0100

    docblocked dog class getters

--- src/Classes/Dog.php
+++ src/Classes/Dog.php
@@ -15,96 +15,96 @@ Class Dog {
     private $countryCode;
     private $description;
 
-    /**
-     * @return mixed
+    /** Function that returns the Bred For property
+     * @return string Bred For property
      */
     public function getBredFor() : string
     {
         return $this->bredFor;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Breed Group property
+     * @return string Breed Group property
      */
     public function getBreedGroup() : string
     {
         return $this->breedGroup;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Country Code property
+     * @return string Country Code property
      */
     public function getCountryCode() : string
     {
         return $this->countryCode;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Description property
+     * @return string Description property
      */
     public function getDescription() : string
     {
         return $this->description;
     }
 
-    /**
-     * @return mixed
+    /**  Function that returns the Height(Imperial) property
+     * @return string Height (Imperial) property
      */
     public function getHeightImperial() : string
     {
         return $this->heightImperial;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Height(Metric) property
+     * @return string Height (Metric) property
      */
     public function getHeightMetric() : string
     {
         return $this->heightMetric;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Life Span property
+     * @return string Life Span property
      */
     public function getLifeSpan() : string
     {
         return $this->lifeSpan;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Name property
+     * @return string Name property
      */
     public function getName() : string
     {
         return $this->name;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Origin property
+     * @return string Origin property
      */
     public function getOrigin() : string
     {
         return $this->origin;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Temperament property
+     * @return string Temperament property
      */
     public function getTemperament() : string
     {
         return $this->temperament;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Weight (Imperial) property
+     * @return string Weight (Imperial) property
      */
     public function getWeightImperial() : string
     {
         return $this->weightImperial;
     }
 
-    /**
-     * @return mixed
+    /** Function that returns the Weight (Metric) property
+     * @return string Weight (Metric) property
      */
     public function getWeightMetric() : string
     {
