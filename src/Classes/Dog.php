110b0292a3851ff01ae5a3a3cf91ab8fdb5df642
xz-dice <xandicearmah@gmail.com>
2021-05-10 16:37:28 +0100

    added getters to dog class and built dog hydrator class

--- src/Classes/Dog.php
+++ src/Classes/Dog.php
@@ -14,5 +14,101 @@ Class Dog {
     private $temperament;
     private $countryCode;
     private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getBredFor() : string
+    {
+        return $this->bredFor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBreedGroup() : string
+    {
+        return $this->breedGroup;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountryCode() : string
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription() : string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeightImperial() : string
+    {
+        return $this->heightImperial;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeightMetric() : string
+    {
+        return $this->heightMetric;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLifeSpan() : string
+    {
+        return $this->lifeSpan;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrigin() : string
+    {
+        return $this->origin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemperament() : string
+    {
+        return $this->temperament;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWeightImperial() : string
+    {
+        return $this->weightImperial;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWeightMetric() : string
+    {
+        return $this->weightMetric;
+    }
 }
 
