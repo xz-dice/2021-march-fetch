53cf69ab0c2325beac9f99b5bc6fef2532698309
onekatytocode <1katytogo@gmail.com>
2021-05-10 15:50:51 +0100

    changed quotes from backticks on vendor autoload

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-require_once ‘vendor/autoload.php’;
+require_once 'vendor/autoload.php';
+
+$url = 'https://dev.io-academy.uk/resources/doggies.json';
+$request = curl_init($url);
+curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($request);
+curl_close($request);
+$thing = json_decode($response, true);
+
+echo '<pre>';
+var_dump($thing);
+echo '</pre>';
+//foreach ($thing['data'] as $dog) {
+//    echo $dog['name'] . '<br>';
+//}
+
