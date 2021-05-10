85263e6589d3989fdccaf436bdcda28c0123d378
MattW-R <Matthew.wr89@gmail.com>
2021-05-10 16:21:14 +0100

    Linked normalize.css & added meta fix

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ require_once 'vendor/autoload.php';
 <html lang="en-GB">
     <head>
         <title>Fetch</title>
+        <link href="css/normalize.css" type="text/css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
 
