5b183a1f0cea63bf1a26f02e90f96d33ccd1ba6b
nacm1269 <nacmuran@gmail.com>
2021-05-11 11:04:53 +0100

    updated the displayMainPage function to include tabbing

--- src/Classes/DogDisplayer.php
+++ src/Classes/DogDisplayer.php
@@ -9,24 +9,24 @@ class DogDisplayer {
      * @return string
      */
 
-    protected static function displayMainPage(Dog $dogs): string {
+    protected static function displayMainPage(array $dogs): string {
         $result = '';
         foreach ($dogs as $dog) {
             $result .= '<article>';
-            $result .= '<div> <img src="$dog->getId()" alt="$dog->getName()"> </div>';
+            $result .= '<div> <img tabindex="2" src="../../assets/dog-images/' . $dog->getId() . '.jpeg" alt="$dog->getName()"></div>';
             $result .= '<div>';
-            $result .= '<h3>$dog->getName()</h3>';
-            $result .= '<p>$dog->getTemperament()</p>';
-            $result .= '<p>$dog->getWeightMetric() kg</p>';
-            $result .= '<a>Fetch!</a>';
+            $result .= '<h3 tabindex="2">' . $dog->getName() . '</h3>';
+            $result .= '<p tabindex="2">' . $dog->getTemperament . '()</p>';
+            $result .= '<p tabindex="2">' . $dog->getWeightMetric() . ' kg</p>';
+            $result .= '<a tabindex="2">Fetch!</a>';
             $result .= '</div>';
         }
         return $result;
     }
 }
 
-$dogs = new Dog
-    $name: "Terry";
-    $temperament: "Fiery";
-    $weightMetric: 500;
+
+$dogId = 6;
+
+
 
