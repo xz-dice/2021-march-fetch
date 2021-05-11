05ebde860565af35732905e9cef4a3d985c040bf
MattW-R <Matthew.wr89@gmail.com>
2021-05-11 10:59:18 +0100

    Refactored index.php specific styling to index.css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require_once 'vendor/autoload.php';
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet" />
         <link href="css/normalize.css" type="text/css" rel="stylesheet" />
         <link href="css/styles.css" type="text/css" rel="stylesheet" />
+        <link href="css/index.css" type="text/css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
