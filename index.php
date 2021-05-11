716352dec2dc4d8c507b62ae37847e6ea18ec6aa
MattW-R <Matthew.wr89@gmail.com>
2021-05-11 11:56:03 +0100

    Removed placeholder articles

--- index.php
+++ index.php
@@ -21,63 +21,7 @@ require_once 'vendor/autoload.php';
             <h4 tabindex="1">An app which gives you a ruff idea of which dog is best for you.</h4>
             <h4 tabindex="1">You'd be barking mad not to try it.</h4>
         </header>
-        <main>
-            <article>
-                <div>
-                    <img tabindex="2" src="assets/dog-images/1.jpeg" />
-                </div>
-                <div>
-                    <h3 tabindex="2">name</h3>
-                    <p tabindex="2">temperament</p>
-                    <p tabindex="2">weight</p>
-                    <a tabindex="2" href="index.php">Fetch!</a>
-                </div>
-            </article>
-            <article>
-                <div>
-                    <img src="assets/dog-images/1.jpeg" />
-                </div>
-                <div>
-                    <h3>name</h3>
-                    <p>temperament</p>
-                    <p>weight</p>
-                    <a>Fetch!</a>
-                </div>
-            </article>
-            <article>
-                <div>
-                    <img src="assets/dog-images/1.jpeg" />
-                </div>
-                <div>
-                    <h3>name</h3>
-                    <p>temperament</p>
-                    <p>weight</p>
-                    <a>Fetch!</a>
-                </div>
-            </article>
-            <article>
-                <div>
-                    <img src="assets/dog-images/1.jpeg" />
-                </div>
-                <div>
-                    <h3>name</h3>
-                    <p>temperament</p>
-                    <p>weight</p>
-                    <a>Fetch!</a>
-                </div>
-            </article>
-            <article>
-                <div>
-                    <img src="assets/dog-images/1.jpeg" />
-                </div>
-                <div>
-                    <h3>name</h3>
-                    <p>temperament</p>
-                    <p>weight</p>
-                    <a>Fetch!</a>
-                </div>
-            </article>
-        </main>
+        <main></main>
         <footer>
             <div>
                 <img src="assets/paw.svg" />
