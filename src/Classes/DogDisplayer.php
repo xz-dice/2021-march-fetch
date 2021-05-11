1066b57e45720d6ab15b21ea02a5b9be4af44e91
SteeleSR <samuelrsteele@gmail.com>
2021-05-11 14:18:15 +0100

    amended dogDisplayer, altered CSS slightly to make dog info and fetch button fit better

--- src/Classes/DogDisplayer.php
+++ src/Classes/DogDisplayer.php
@@ -13,13 +13,13 @@ class DogDisplayer {
         $result = '';
         foreach ($dogs as $dog) {
             $result .= '<article>';
-            $result .= '<div> <img tabindex="2" src="assets/dog-images/' . $dog->getId() . '.jpeg" alt="' . $dog->getName() .'"></div>';
             $result .= '<div>';
+            $result .= '<img tabindex="2" src="assets/dog-images/' . $dog->getId() . '.jpeg" alt="' . $dog->getName() .'">';
             $result .= '<h3 tabindex="2">' . $dog->getName() . '</h3>';
             $result .= '<p tabindex="2">' . $dog->getTemperament() . '</p>';
             $result .= '<p tabindex="2">' . $dog->getWeightMetric() . ' kg</p>';
-            $result .= '<a tabindex="2" aria-label="click here for more info about this dog">Fetch!</a>';
             $result .= '</div>';
+            $result .= '<div><a tabindex="2" aria-label="click here for more info about this dog">Fetch!</a></div>';
             $result .= '</article>';
         }
         return $result;
