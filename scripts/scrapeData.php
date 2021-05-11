9a1f3bb5283477e0e26a5b302b6c7ce41e172365
emmatrain <emtrain7@gmail.com>
2021-05-11 10:21:17 -0300

    WIP script

--- scripts/scrapeData.php
+++ scripts/scrapeData.php
@@ -9,20 +9,45 @@ $response = curl_exec($request);
 curl_close($request);
 $dogs = json_decode($response, true);
 
-echo '<pre>';
-var_dump($dogs);
-echo '</pre>';
 
-// prepare the VALUES statement to put into the database
+//echo '<pre>';
+//var_dump($dogs);
+//echo '</pre>';
+//
+
+//$result = '';
+//foreach ($dogs['data'] as $dog) {
+//    $result .= '(';
+//    foreach ($dog as $key => $value) {
+//        if (gettype($value) == 'array') {
+//            foreach ($value as $subKey => $subValue) {
+//                $result .= "'" . str_replace("'", "\'", $value[$subKey]) . "'" . ', ';
+//            }
+//        } else {
+//            $result .= "'" . str_replace("'", "\'", $dog[$key]) . "'" . ', ';
+//        }
+//    }
+//    $result = trim($result, ",");
+//    $result .= '),';
+//}
+
+
+//var_dump ($dogs);
+
 $result = '';
-
 foreach ($dogs['data'] as $dog) {
-    $result .= '(' . "'" . $dog['id'] . "'" . ', ' . "'" . $dog['name'] . "'" . ', ' . "'" . $dog['temperament'] . "'" . ', ' . "'" . $dog['weight']['imperial'] . "'" . ', ' .  "'" . $dog['weight']['metric'] . "'" . ', ' . "'" . $dog['height']['imperial'] . "'" . ', ' . "'" . $dog['height']['metric'] . "'" . ', ' . "'" . $dog['bred_for'] . "'" . ', ' . "'" . $dog['bred_group'] . "'" . ', ' . "'" . $dog['life_span'] . "'" . ', ' . "'" . $dog['origin'] . "'" . ', ' . "'" . $dog['country_code'] . "'" . ', ' . "'" . $dog['description'] . "'" . '),';
-}
+    if (!$dog) {
+        continue;
+    } else {
+        $result .= '(' . "'" . @$dog['id'] . "'" . ', ' . "'" . @$dog['name'] . "'" . ', ' . "'" . @$dog['temperament'] . "'" . ', ' . "'" . @$dog['weight']['imperial'] . "'" . ', ' .  "'" . @$dog['weight']['metric'] . "'" . ', ' . "'" . @$dog['height']['imperial'] . "'" . ', ' . "'" . @$dog['height']['metric'] . "'" . ', ' . "'" . @$dog['bred_for'] . "'" . ', ' . "'" . @$dog['breed_group'] . "'" . ', ' . "'" . @$dog['life_span'] . "'" . ', ' . "'" . @$dog['origin'] . "'" . ', ' . "'" . @$dog['country_code'] . "'" . ', ' . "'" . str_replace("'", "\'", @$dog['description']) . "'" . '),';
+    }}
 
 $trimmedResult = trim($result, ",");
-
 echo $trimmedResult;
+
+
+
+
 
 $db = new PDO('mysql:host=db; dbname=fetch', 'root', 'password');
 $query = $db->prepare("INSERT INTO `dogs` (`id`, `name`, `temperament`, `weight_imperial`, `weight_metric`, `height_imperial`, `height_metric`, `bred_for`, `breed_group`, `life_span`, `origin`, `country_code`, `description`) VALUES $trimmedResult;");
