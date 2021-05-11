c4dff149cd9aac6080a9acf8ae815f6fb893717a
nacm1269 <nacmuran@gmail.com>
2021-05-11 09:52:37 +0100

    doc blocked displaymainpage function

--- src/Classes/DogDisplayer.php
+++ src/Classes/DogDisplayer.php
@@ -1,15 +1,21 @@
 <?php
 
 class DogDisplayer {
+
+    /** Function to generate a long string to output dog articles to main page
+     * @param array $dogs
+     * @return string
+     */
+
     protected static function displayMainPage(array $dogs): string {
         $result = '';
         foreach ($dogs as $dog) {
             $result .= '<article>';
-            $result .= '<div> <img src="$dog->getId" alt="$dog->getName()"> </div>';
+            $result .= '<div> <img src="$dog->getId()" alt="$dog->getName()"> </div>';
             $result .= '<div id="">';
             $result .= '<h3>$dog->getName()</h3>';
             $result .= '<p>$dog->getTemperament()</p>';
-            $result .= '<p>$dog->getWeight() kg</p>';
+            $result .= '<p>$dog->getWeightMetric() kg</p>';
             $result .= '<a href="">Fetch!</a>';
             $result .= '</div>';
         }
