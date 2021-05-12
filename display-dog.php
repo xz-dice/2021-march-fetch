f3d9e97eb5fae6bb04e5b6d2dea19911d283eb53
MattW-R <Matthew.wr89@gmail.com>
2021-05-12 11:06:38 +0100

    Created display-dog.php, linked styles.css (etc), created a main section that will contain image & dog info. Included header.php & footer.php.

---
+++ display-dog.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<html lang="en-GB">
+    <head>
+        <title>Fetch</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&display=swap" rel="stylesheet" />
+        <link href="css/normalize.css" type="text/css" rel="stylesheet" />
+        <link href="css/styles.css" type="text/css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body>
+        <?php include_once 'src/Prefabs/header.php' ?>
+        <main>
+            
+        </main>
+        <?php include_once 'src/Prefabs/footer.php' ?>
+    </body>
+</html>
