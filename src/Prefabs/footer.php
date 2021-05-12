e35595f38870cc75f963668538d7a31a169b7d97
MattW-R <Matthew.wr89@gmail.com>
2021-05-12 10:48:56 +0100

    Created header.php & footer.php to hold respective html content & included into index.php.

---
+++ src/Prefabs/footer.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '<footer>
+            <div>
+                <img src="assets/paw.svg" />
+            </div>
+            <div>
+                <p tabindex="3">This page will lead to a best friend you want to stick by.</p>
+                <p tabindex="3">Companion app coming soon.</p>
+            </div>
+        </footer>';
+
+?>
