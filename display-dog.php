0347a463ccaa37a8d42d8414fb3c7d69b114cd1e
khorh <helen.khor@googlemail.com>
2021-05-12 11:32:02 +0100

    added code to connect to database and front-end

--- display-dog.php
+++ display-dog.php
@@ -1,1 +1,11 @@
-hello Helen!
+<?php
+
+require_once 'vendor/autoload.php';
+$db = new \Fetch\Classes\Db();
+$dogID = $_GET['id'];
+//This proves our code works
+echo $dogID;
+//$dogs = \Fetch\Hydrators\DogHydrator::getDogDetail($db->getDb(), $dogID);
+//$displayDogs = \Fetch\Classes\DogDisplayer::displayDetailedInfo($dogs[0]);
+
+?>
