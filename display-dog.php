95efbc8741a853c338a279c6e6ac508b9471bf9e
xz-dice <xandicearmah@gmail.com>
2021-05-12 12:24:20 +0100

    added docblock to getSingleDog function

--- display-dog.php
+++ display-dog.php
@@ -4,7 +4,8 @@ require_once 'vendor/autoload.php';
 $db = new \Fetch\Classes\Db();
 $dogID = $_GET['Id'];
 $dogs = \Fetch\Hydrators\DogHydrator::getSingleDog($db->getDb(), $dogID);
-var_dump($dogs);
+//var_dump($dogs);
 //$displayDogs = \Fetch\Classes\DogDisplayer::displayDetailedInfo($dogs[0]);
 
+echo gettype($dogID);
 ?>
