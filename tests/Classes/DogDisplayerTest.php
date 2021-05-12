f678084c83e20fb489872f49e90ac6796412a343
khorh <helen.khor@googlemail.com>
2021-05-12 10:11:55 +0100

    removed fetch button from the test

--- tests/Classes/DogDisplayerTest.php
+++ tests/Classes/DogDisplayerTest.php
@@ -8,7 +8,7 @@ use Fetch\Classes\Dog;
 
 class DogDisplayerTest extends TestCase {
     public function testDisplayMainPageSuccess() {
-        $expected = '<article><div><img tabindex="2" src="assets/dog-images/2.jpeg" alt="Cuthbert"><h3 tabindex="2">Cuthbert</h3><p tabindex="2">Happy</p><p tabindex="2">2-3 kg</p></div><div><a tabindex="2" aria-label="click here for more info about this dog">Fetch!</a></div></article>';
+        $expected = '<article><div><img tabindex="2" src="assets/dog-images/2.jpeg" alt="Cuthbert"><h3 tabindex="2">Cuthbert</h3><p tabindex="2">Happy</p><p tabindex="2">2-3 kg</p></div></article>';
         $dogMock = $this->createMock(Dog::class);
         $dogMock->expects($this->once())
             ->method('getId')
