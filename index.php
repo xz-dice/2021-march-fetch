edb3533daa50bf5475466c6275d14f855242ce0d
nacm1269 <nacmuran@gmail.com>
2021-05-13 15:54:02 +0100

    amended get request to display dog temperament in url

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
 require_once 'vendor/autoload.php';
+//header('Location: index.php');
 $db = new \Fetch\Classes\Db();
-$dogs = \Fetch\Hydrators\DogHydrator::getDogs($db->getDb());
-$displayDogs = \Fetch\Classes\DogDisplayer::displayMainPage($dogs);
+
+if (isset($_GET['temperament']) && is_string($_GET['temperament'])) {
+    $dogTemperament = strtolower($_GET['temperament']);
+    $dogs = \Fetch\Hydrators\DogHydrator::getDogs($db->getDb());
+    $displayDogs = \Fetch\Classes\DogDisplayer::displayMainPage($dogs, $dogTemperament);
+} else {
+    $dogs = \Fetch\Hydrators\DogHydrator::getDogs($db->getDb());
+    $displayDogs = \Fetch\Classes\DogDisplayer::displayMainPage($dogs);
+}
 
 ?>
 
