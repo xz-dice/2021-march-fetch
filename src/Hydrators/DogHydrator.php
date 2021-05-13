4b772142ab4074056ca429eefd649e9279c4a6d6
MattW-R <Matthew.wr89@gmail.com>
2021-05-13 10:46:21 +0100

    Updated getDogs doc block

--- src/Hydrators/DogHydrator.php
+++ src/Hydrators/DogHydrator.php
@@ -5,9 +5,10 @@ use Fetch\Classes\Dog;
 
 Class DogHydrator {
 
-    /** Function to retrieve all dogs with all data from database
-     * @param \PDO $db takes a parameter that is the database of dogs
-     * @return array of dog objects from database
+    /** Function to retrieve all/a filtered selection of dogs with all their data from database
+     * @param \PDO $db a PDO connection to the database of dogs
+     * @param string $dogTemperament an optional dog temperament string to filter the retrieved dogs
+     * @return array array of dog objects from the database
      */
     public static function getDogs(\PDO $db, string $dogTemperament = ''): array{
         $query = $db->prepare("SELECT `id`, `name`, `temperament`, `weight_imperial` AS 'weightImperial', `weight_metric` AS 'weightMetric', `height_imperial` AS 'heightImperial', `height_metric` AS 'heightMetric', `bred_for` AS 'bredFor', `breed_group` AS 'breedGroup', `life_span` AS 'lifeSpan', `origin`, `country_code` AS 'countryCode', `description` FROM `dogs` WHERE `temperament` LIKE '%$dogTemperament%';");
