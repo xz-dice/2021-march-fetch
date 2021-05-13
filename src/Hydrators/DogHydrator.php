2f88040260dd30d50ffdc1ec6313eefc9b8bf214
MattW-R <Matthew.wr89@gmail.com>
2021-05-13 10:59:24 +0100

    Bound dogTemperament filter withing getDogs

--- src/Hydrators/DogHydrator.php
+++ src/Hydrators/DogHydrator.php
@@ -11,7 +11,8 @@ Class DogHydrator {
      * @return array array of dog objects from the database
      */
     public static function getDogs(\PDO $db, string $dogTemperament = ''): array{
-        $query = $db->prepare("SELECT `id`, `name`, `temperament`, `weight_imperial` AS 'weightImperial', `weight_metric` AS 'weightMetric', `height_imperial` AS 'heightImperial', `height_metric` AS 'heightMetric', `bred_for` AS 'bredFor', `breed_group` AS 'breedGroup', `life_span` AS 'lifeSpan', `origin`, `country_code` AS 'countryCode', `description` FROM `dogs` WHERE `temperament` LIKE '%$dogTemperament%';");
+        $query = $db->prepare("SELECT `id`, `name`, `temperament`, `weight_imperial` AS 'weightImperial', `weight_metric` AS 'weightMetric', `height_imperial` AS 'heightImperial', `height_metric` AS 'heightMetric', `bred_for` AS 'bredFor', `breed_group` AS 'breedGroup', `life_span` AS 'lifeSpan', `origin`, `country_code` AS 'countryCode', `description` FROM `dogs` WHERE `temperament` LIKE CONCAT('%', :DogTemperament, '%');");
+        $query->bindParam(':DogTemperament', $dogTemperament);
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS, \Fetch\Classes\Dog::class);
         return $query->fetchAll();
