d6d123440f2a85bc9b5c7cde26b78d3da54f3a8c
SteeleSR <samuelrsteele@gmail.com>
2021-05-13 16:06:23 +0100

    corrected code review changes

--- src/Classes/FilterButtonsDisplayer.php
+++ src/Classes/FilterButtonsDisplayer.php
@@ -4,12 +4,19 @@ namespace Fetch\Classes;
 
 class FilterButtonsDisplayer {
 
+    /** Iterates through temperamentsArray and generates temperament tags
+     *
+     * @param array $temperamentsArray array containing all unique temperaments
+     * @param string $currentTemperament temparement that user has filtered for
+     *
+     * @return string of html to output filter tags to front end
+     */
     public static function displayFilterButtons(array $temperamentsArray, string $currentTemperament = ''): string
     {
         $result = '';
         foreach ($temperamentsArray as $temperament) {
             if(gettype($temperament) === 'string') {
-                $result .= '<a href="index.php" class="chip';
+                $result .= '<a href="index.php" tabindex="2" aria-label="Search for dogs that are ' . $temperament . '" class="chip';
                 if($currentTemperament === $temperament) {
                     $result .= ' active-temperament';
                 }
