13a603e2a1e73f6306c319ac835ff9cfbc0dd5af
MattW-R <Matthew.wr89@gmail.com>
2021-05-13 10:39:42 +0100

    Refactored getFilteredDogs into getDogs

--- src/Hydrators/DogHydrator.php
+++ src/Hydrators/DogHydrator.php
@@ -9,8 +9,8 @@ Class DogHydrator {
      * @param \PDO $db takes a parameter that is the database of dogs
      * @return array of dog objects from database
      */
-    public static function getDogs(\PDO $db): array{
-        $query = $db->prepare("SELECT `id`, `name`, `temperament`, `weight_imperial` AS 'weightImperial', `weight_metric` AS 'weightMetric', `height_imperial` AS 'heightImperial', `height_metric` AS 'heightMetric', `bred_for` AS 'bredFor', `breed_group` AS 'breedGroup', `life_span` AS 'lifeSpan', `origin`, `country_code` AS 'countryCode', `description` FROM `dogs`;");
+    public static function getDogs(\PDO $db, string $dogTemperament = ''): array{
+        $query = $db->prepare("SELECT `id`, `name`, `temperament`, `weight_imperial` AS 'weightImperial', `weight_metric` AS 'weightMetric', `height_imperial` AS 'heightImperial', `height_metric` AS 'heightMetric', `bred_for` AS 'bredFor', `breed_group` AS 'breedGroup', `life_span` AS 'lifeSpan', `origin`, `country_code` AS 'countryCode', `description` FROM `dogs` WHERE `temperament` LIKE '%$dogTemperament%';");
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS, \Fetch\Classes\Dog::class);
         return $query->fetchAll();
@@ -27,16 +27,5 @@ Class DogHydrator {
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS, \Fetch\Classes\Dog::class);
         return [$query->fetch()];
-    }
-
-    /** Function to retrieve all dogs with all data from database
-     * @param \PDO $db takes a parameter that is the database of dogs
-     * @return array of dog objects from database
-     */
-    public static function getFilteredDogs(\PDO $db, string $dogTemperament): array{
-        $query = $db->prepare("SELECT `id`, `name`, `temperament`, `weight_imperial` AS 'weightImperial', `weight_metric` AS 'weightMetric', `height_imperial` AS 'heightImperial', `height_metric` AS 'heightMetric', `bred_for` AS 'bredFor', `breed_group` AS 'breedGroup', `life_span` AS 'lifeSpan', `origin`, `country_code` AS 'countryCode', `description` FROM `dogs` WHERE `temperament` LIKE '%$dogTemperament%';");
-        $query->execute();
-        $query->setFetchMode(\PDO::FETCH_CLASS, \Fetch\Classes\Dog::class);
-        return $query->fetchAll();
     }
 }
